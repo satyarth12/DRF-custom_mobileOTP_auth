0298b32507aefec5d6e89cdbc1e3a31e08dbbe41
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2021-12-04 15:01:10 +0330

    Add rest_framework to INSTALLED_APPS

--- OTP_auth/settings.py
+++ OTP_auth/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'custom_users',
-    'knox'
+    'knox',
+    'rest_framework'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
